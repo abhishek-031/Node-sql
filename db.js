89d6e53bbffd0312ad75cb79d994c4e2e05bbb2d
Abhishek <abhishekv@geekyants.com>
2021-04-17 17:45:01 +0530

    db setup

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 
+// change credentitals here for your mysql server
 const dbCredentials = {
     host: 'localhost',
     user: 'root',
@@ -13,5 +14,12 @@ connection.connect(error => {
     if(error) throw error;
     console.log("successfully connected to database");
 })
+
+connection.query(`CREATE TABLE IF NOT EXISTS Users ( 
+    id int(11) not null primary key auto_increment,
+    name varchar(50),
+    email varchar(50),
+    phone varchar(12),
+    address varchar(255) )`);
 
 module.exports = connection;
